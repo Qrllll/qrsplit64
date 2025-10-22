af267ba42350bcec5b7eebc585d43cec1e160d76
Qrllll <qirui0802@gmail.com>
2025-10-22 12:23:08 +0800

    Refactor keymap.c to update RGB settings and layers

--- qmk/keymaps/default/keymap.c
+++ qmk/keymaps/default/keymap.c
@@ -145,6 +145,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_P7, KC_P8, KC_P9, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
             KC_P4, KC_P5, KC_P6, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
             KC_P1, KC_P2, KC_P3, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-            KC_NO, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS
+            KC_NO, KC_P0, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS
         )
+
 };
